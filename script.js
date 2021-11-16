01166422a3b13c48d17d1a2a17866e2db19d5555
Natalya Myunster <natalyamyunster@yahoo.com>
2021-11-16 17:14:14 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function func() {
+   alert('Sophie');
+}
+
+func();
+
+function func1(){
+   let sum = 0;
+   for(let i = 1; i <= 100; i++){
+       sum += i;
+
+   }
+   alert(sum);
+}
+
+func1();
